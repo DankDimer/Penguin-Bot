f719ca859abbf0efca437e704f29095bc3ee2650
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-11-16 23:35:43 -0500

    Fixes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // https://trello.com/b/KZ0Dt5ww/penguin-bot
 // 0.0.0 (MAJOR RELEASE).(SMALL FEATURES).(BUG FIXES AND SMALLER CHANGES)
 //********************************************************************************************************
-const discord = require("discord.js");
+const Discord = require("discord.js");
 const fs = require("fs");
+const discord = require("discord.js");
 //----------------------------------------------------------------------------
 // -------------------------------------------
 const colors = require("./colors.json")
