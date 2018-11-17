6f56170d60449c4380da05b97d5869bdb830232a
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-11-16 23:21:44 -0500

    Base Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// https://trello.com/b/KZ0Dt5ww/penguin-bot
+// 0.0.0 (MAJOR RELEASE).(SMALL FEATURES).(BUG FIXES AND SMALLER CHANGES)
+//============================================================================
+const discord = require("discord.js");
+const fs = require("fs");
