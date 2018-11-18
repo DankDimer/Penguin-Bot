0f27c195116ded13a2bf657a9965a27a9d41efcc
TheCow <cow2cow4cow@gmail.com>
2018-11-18 00:04:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ bot.on("message", async message => {
     if (message.mentions.channels.first()) {
         message.mentions.channels.forEach(function(channel) {
             message.guild.members.forEach(function(member) {
-                if (message.channel.permissionsFor(member).has("VIEW_MESSAGES")) {
+                if (message.channel.permissionsFor(member).has("VIEW_CHANNEL")) {
                     let embed = new Discord.RichEmbed()
                     embed.color = 16007775
                     embed.setAuthor("Pinged Alert!", "https://i.imgur.com/ZrT9MM0.png")
