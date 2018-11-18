216c8f5921e0627a34d189c217f8c58c3b880bb7
TheCow <cow2cow4cow@gmail.com>
2018-11-17 23:36:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,9 @@ bot.on("message", async message => {
     if (message.mentions.roles.first()) {
         message.mentions.roles.forEach(function(role) {
             message.guild.members.forEach(function(member) {
-                if (member.roles.find(member => member.role === role.toString())) {
+                var rolex = member.roles.find(rolex => rolex.name === role.toString)
+
+                if (member.roles.find()) {
                     let embed = new Discord.RichEmbed()
                     embed.color = 16007775
                     embed.setAuthor("Pinged Alert!", "https://i.imgur.com/ZrT9MM0.png")
